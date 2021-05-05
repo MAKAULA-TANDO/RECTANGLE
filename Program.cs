26494ecf228caf6a2cd2ecc9df3d594052b7631c
MAKAULA-TANDO <217291163@mywsu.ac.za>
2021-05-05 12:03:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace RECTANGLE
+{
+    public class Rectangle
+    {
+
+        public Double Length;
+        public Double Width;
+
+        public Rectangle()
+        {
+            Length = 0.0;
+            Width = 0.0;
+        }
+        public Rectangle(Double Len, Double Wid)
+        {
+            Length = Len;
+            Width = Wid;
+        }
+
+        public void input_Attributes()
+        {
+            Console.WriteLine("Enter the length :");
+            Length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the width :");
+            Width = Convert.ToDouble(Console.ReadLine());
+
+        }
+        public Double calculate_Perimeter(Double len, Double wid)
+        {
+            return 2 * (len + wid);  
+        }
+        public Double calculate_Area(Double len, Double wid)
+        {
+            return len* wid;
+        }
+
+
+        public void output_results()
+        {
+            Console.WriteLine("The area of a rectangle :" + calculate_Area(Length, Width));
+            Console.WriteLine("The perimeter of a rectangle :" + calculate_Perimeter(Length, Width));
+            Console.ReadKey();
+        }
+
+
+        class Program 
+        {
+            static void Main(string[] args)
+            {
+                Rectangle Obj = new Rectangle();
+                Obj.input_Attributes();
+                Obj.output_results();
+            }
+        }
+    }
+}
